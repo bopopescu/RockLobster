5869e85a55d27f471f268bd1a883887f6f9c2ae9
Michał Dobrzyński <michal@paddle.com>
2019-10-03 13:10:09 +0100

    Add URL parser function logic.

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import requests
 from requests.exceptions import HTTPError
 
-inbound_urlcall_from_kustomer = "http://webhook.site/2f8dcb04-ce40-493b-b8c5-30570bb2fa2e?message=What%20happened%20to%20my%20payout%3F"
-swiftype_engine_search_query = "checkout"
-
-
 swiftype_engine_urls = {
     "faq": "https://search-api.swiftype.com/api/v1/engines/paddle-faq/search.json",
-    # "documentation": "https://search-api.swiftype.com/api/v1/engines/paddle-documentation/search.json",
-    # "blog": "https://search-api.swiftype.com/api/v1/engines/paddle-blog/search.json",
-    # "developer": "https://search-api.swiftype.com/api/v1/engines/paddle-developer/search.json",
+    "documentation": "https://search-api.swiftype.com/api/v1/engines/paddle-documentation/search.json",
+    "blog": "https://search-api.swiftype.com/api/v1/engines/paddle-blog/search.json",
+    "developer": "https://search-api.swiftype.com/api/v1/engines/paddle-developer/search.json",
 
 }
 
 swiftype_call_params = {
     "auth_token": "6oMTu5o6HVtFMPjbHTDJ",
-    "q": swiftype_engine_search_query
+    "q": ""
 }
 
 
 def get_message_from_kustomer_call(event, context):
-    # body = event['queryStringParameters']
-    swiftype_call_params["q"] = body['message']
+    message_body = event['queryStringParameters']
+    swiftype_call_params["q"] = message_body['message']
+    print(swiftype_call_params["q"])
 
 
 
@@ -29,4 +26,4 @@ def call_swiftype_search(url):
     response = requests.get(url, swiftype_call_params)
     return response.content
 
-print(call_swiftype_search("https://search-api.swiftype.com/api/v1/engines/paddle-faq/search.json"))
+print(call_swiftype_search(swiftype_engine_urls["faq"]))
