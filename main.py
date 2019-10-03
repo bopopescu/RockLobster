284c3cdba11f2a3bce1b4e65bac8dbcaa833fd1a
Michał Dobrzyński <michal@paddle.com>
2019-10-03 12:05:31 +0100

    Add new package for lambda, based on existing one.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 from requests.exceptions import HTTPError
 
+inbound_urlcall_from_kustomer = "http://webhook.site/2f8dcb04-ce40-493b-b8c5-30570bb2fa2e?message=What%20happened%20to%20my%20payout%3F"
 swiftype_engine_search_query = "checkout"
+
 
 swiftype_engine_urls = {
     "faq": "https://search-api.swiftype.com/api/v1/engines/paddle-faq/search.json",
@@ -18,5 +20,6 @@ swiftype_call_params = {
 
 def call_swiftype_search(url):
     response = requests.get(url, swiftype_call_params)
-    return response
+    return response.content
 
+print(call_swiftype_search(swiftype_engine_urls["faq"]))
